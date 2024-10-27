0154901cd03b64cdb278ace322b3aa539bb1e1ae
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-27 14:29:49 +0530

    Server running

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
-const app=express()
+const app = express();
+dotenv.config({ path: './config/config.env' });
+
+app.use(cors()); // Don't forget to use CORS if needed
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,7 @@
-import app from './app';
+import app from './app.js';
 
+const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+
+app.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON PORT ${PORT}`);
+});
