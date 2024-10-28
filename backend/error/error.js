ea58bb68cec12eabecf3d2225c4e1e16b16f9afc
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-28 19:22:26 +0530

    MAKE IT WORK FOR ERROR

--- backend/error/error.js
+++ backend/error/error.js
@@ -1,3 +1,18 @@
 class ErrorHandler extends Error{
-    
+
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode=statusCode;
+
+    }
 }
+export const errorMiddleware =(err,req,res,next)=>{
+    err.message=err.message || "Internal Server Erroe!"
+    err.statusCode=err.statusCode || 500;
+
+    return res.status(err.statusCode).json({
+        successs:false,
+        message:error.message,
+
+    });
+};
