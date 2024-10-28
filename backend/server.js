2f8b1210b909ac49ca7ddd0b96fa4f688fc78d0b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-28 10:48:48 +0530

    wokring on it

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
 app.listen(PORT, () => {
-    console.log(`SERVER RUNNING ON PORT ${PORT}`);
+    console.log(`SERVER RUNNING ON PO ${PORT}`);
 });
