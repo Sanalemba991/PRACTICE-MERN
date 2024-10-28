d55020cee4d15007cf4749b84d5e6756aa4772f4
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-28 10:48:35 +0530

    making work

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { dbConnection } from './database/dbConnection.js';
+
+
 
 const app = express();
+
+// Load environment variables from config file
 dotenv.config({ path: './config/config.env' });
 
+// Connect to the database
+dbConnection();
+
+// CORS configuration
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["POST"],
-    credentials:true,
-})); // Don't forget to use CORS if needed
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+}));
 
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-
-
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
