ee38579bf1ad3f5363b5a30a3fb8fa9b83f13628
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-30 19:37:17 +0530

    hot toast add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
-
+import {Toaster} from "react-hot-toast"
 
 function App() {
   return (
