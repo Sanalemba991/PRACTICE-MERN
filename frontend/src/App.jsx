9b0aba19e0b4392a4af4de33d782bfb4155bb10f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-30 19:33:59 +0530

    fronend set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
