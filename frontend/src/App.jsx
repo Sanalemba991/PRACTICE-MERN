8f38b2d6310c13adf7840174326870041f5f2096
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-30 19:47:39 +0530

    react hot toast download

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Success from './pages/Success';
 
 function App() {
   return (
    <Router>
     <Routes>
-    <Route path="/" element={}/>
-    <Route path="/" element={}/>
-    <Route path="/" element={}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/success" element={<Success/>}/>
+    <Route path="*" element={<NotFound/>}/>
     </Routes>
     <Toaster/>
    </Router>
