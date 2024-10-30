b599991ee0cfe42ea5d9ea529886c1bd149101b1
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-30 19:35:12 +0530

    making work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+   <Router>
+    <Route path="/" element={}/>
+    <Route path="/" element={}/>
+    <Route path="/" element={}/>
+   </Router>
   )
 }
 
