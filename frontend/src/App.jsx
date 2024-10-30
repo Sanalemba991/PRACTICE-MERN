10f8fa8c7320961f487747cee0e625661cce2fed
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-30 19:40:52 +0530

    toaster make it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
-import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
-import {Toaster} from "react-hot-toast"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
    <Router>
+    <Routes>
     <Route path="/" element={}/>
     <Route path="/" element={}/>
     <Route path="/" element={}/>
+    </Routes>
+    <Toaster/>
    </Router>
   )
 }
