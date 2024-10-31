0dc7b55e7b971d21535ca812c5e8c3d095e94021
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-31 22:11:16 +0530

    not finixshed

---
+++ frontend/src/components/Qualities.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {data} from'../restApi.json';
+
+function Qualities() {
+  return (
+  <section className='qualities' id='qualities'>
+<div className='container'>
+
+</div>
+  </section>
+  )
+}
+
+export default Qualities
