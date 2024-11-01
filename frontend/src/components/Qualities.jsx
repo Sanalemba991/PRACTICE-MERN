06890ba514a5012c348755c141964de8f42242cc
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-01 10:22:47 +0530

    add image

--- frontend/src/components/Qualities.jsx
+++ frontend/src/components/Qualities.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import {data} from'../restApi.json';
-
-function Qualities() {
+import {data} from '../restApi.json'
+const Qualities = () => {
   return (
-  <section className='qualities' id='qualities'>
-<div className='container'>
-
-</div>
-  </section>
+    <>
+        <section className='qualities' id='qualities'>
+          <div className="container">
+            {
+              data[0].ourQualities.map(element=>{
+                return(
+                  <div className='card' key={element.id}>
+                      <img src={element.image} alt={element.title} />
+                      
+                     
+                  </div>
+                )
+              })
+            }
+          </div>
+        </section>
+    </>
   )
 }
 
