5e2a4de14c3a4c50a0ef9b0beb486bbec6ad6227
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-01 10:44:17 +0530

    image added

--- frontend/src/components/WhoAreWe.jsx
+++ frontend/src/components/WhoAreWe.jsx
@@ -7,7 +7,7 @@ const WhoAreWe = () => {
       <section className="who_are_we" id="who_are_we">
         <div className="container">
           <div className="text_banner">
-            {data[0].who_we_are.slice(0,2).map((element) => (
+            {data[0].who_we_are.slice(0, 2).map((element) => (
               <div className="card" key={element.id}>
                 <h1 className="heading" style={{ fontWeight: "300" }}>
                   {element.number}
@@ -16,8 +16,21 @@ const WhoAreWe = () => {
               </div>
             ))}
           </div>
-         
-          
+
+          <div className="image_banner">
+            <img className="gradient_bg" src="center.svg" alt="gradientBg" />
+            <img src="whoweare.png" alt="food" />
+          </div>
+          <div className="text_banner">
+            {data[0].who_we_are.slice(2).map((element) => (
+              <div className="card" key={element.id}>
+                <h1 className="heading" style={{ fontWeight: "300" }}>
+                  {element.number}
+                </h1>
+                <p>{element.title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
