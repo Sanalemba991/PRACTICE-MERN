a0c19986cf31f530142951607a8459d1cfca10c8
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-01 10:43:35 +0530

    slice

--- frontend/src/components/WhoAreWe.jsx
+++ frontend/src/components/WhoAreWe.jsx
@@ -7,7 +7,7 @@ const WhoAreWe = () => {
       <section className="who_are_we" id="who_are_we">
         <div className="container">
           <div className="text_banner">
-            {data[0].who_we_are.map((element) => (
+            {data[0].who_we_are.slice(0,2).map((element) => (
               <div className="card" key={element.id}>
                 <h1 className="heading" style={{ fontWeight: "300" }}>
                   {element.number}
