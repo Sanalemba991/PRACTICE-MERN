7ec536037699adeacf1c3ca5c9a9b4d483c9570a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-01 19:34:00 +0530

    fetchong process

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {axios} from 'axios';
 
 function Reservation() {
+  const [ firstName,setFirstName]=useState("");
+  const [ lastName,setLastName]=useState("");
+  const [ email,setEmail]=useState("");
+  const [ date,serDate]=useState("");
+  const [  time,setTime]=useState("");
+  const [ phone,setPhone]=useState("");
+
+  const navigate=useState()
+
+
+  const handleSubmit=async()=>{
+    e.preventDefault();
+    try{
+      const {data}=await axios.post("localhost:5000/api/v1/reservation/send",
+        { firstName, lastName, email, phone, date, time },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setFirstName("");
+      setLastName("");
+      setPhone(0);
+      setEmail("");
+      setTime("");
+      setDate("");
+      navigate("/success");
+    } 
+catch(error){
+
+}
+  }
   return (
-    <div>
-      
-    </div>
+<section className='reservation' id='reservation'>
+
+</section>
   )
 }
 
