855154ad6608552dd979fe499130b50c468f5e15
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-01 10:48:27 +0530

    ok

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -10,7 +10,17 @@ const Team = () => {
           Our team is dedicated to excellence. We are passionate food lovers, committed to using fresh, high-quality ingredients. Each member plays a vital role, from chefs crafting delicious dishes to friendly staff ensuring a great experience. Together, we strive to deliver outstanding service and unforgettable flavors!
           </p>
         </div>
-      
+        <div className="team_container">
+          {data[0].team.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <img src={element.image} alt={element.name} />
+                <h3>{element.name}</h3>
+                <p>{element.designation}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
